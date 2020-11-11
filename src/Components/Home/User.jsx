c3e9af265d257f8abc32faa6e56a928b09613414
josselynVal <josselyn.valenciap@gmail.com>
2020-11-11 02:32:25 -0600

    mi primer commit

---
+++ src/Components/Home/User.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const User = () => (
+  <React.Fragment>
+    <p><h1><strong>INFORMACIÓN GENERAL DEL USUARIO</strong></h1></p>
+  </React.Fragment>
+)
+
+export default User
